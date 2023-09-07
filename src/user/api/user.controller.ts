dff77bb1861a04b89c4bc8c3cb26124c444a2c3f
Renan Santos <renans07@hotmail.com>
2023-09-07 12:41:00 -0300

    adicionando end-points

--- src/user/api/user.controller.ts
+++ src/user/api/user.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/v1/user')
+export class UserController {
+
+    @Post()
+    async createUser(@Body() data){
+        return data;
+    }
+
+    @Put(":id")
+    async updateUser(@Param() params, @Body() data){
+        return {
+            params,
+            data
+        }
+    }
+
+    @Patch(":id")
+    async patchUser(@Param() params, @Body() data){
+        return {
+            data,
+            params
+        }
+    }
+
+    @Patch("inactivate/:id")
+    async inactivateUser(@Param() params, @Body() data){
+        return {
+            data,
+            params
+        }
+    }
+
+    @Delete(":id")
+    async removeUser(@Param() params){
+        return params;
+    }
+
+    @Get()
+    async getUsers(){
+        return []
+    }
+
+    @Get(":id")
+    async getUser(@Param() params){
+        return params
+    }
+}
